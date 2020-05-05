51b6c8f248a0b343e4517f9ad0eb00ec118293e1
LanaKonijnenbelt <62058784+LanaKonijnenbelt@users.noreply.github.com>
2020-05-05 22:37:04 +0200

    W1D2-4

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-console.log("Hallo Winc Academy studenten")
-//Dit is een grote som
-/* console.log(1230941*1823794);
-console.log(637263/54) */
+const age = 22;
+let isFemale = true;
+const driverStatus = "drunk";
+
+
+if (age >= 18 && isFemale){
+
+    console.log('Welkom op onze Ladies Night Party!')
+
+} else{
+
+    console.log('Sorry, je komt er niet in.');
+
+}
+
+if (driverStatus === "bob"){
+
+    console.log('Je bent nog nuchter, wat goed! Je mag zelfstandig naar huis rijden.')
+
+} else{
+
+    console.log('Je hebt te veel alcohol op om nog veilig te kunnen rijden. Neem de nachtbus of een taxi.')
+
+}
+
